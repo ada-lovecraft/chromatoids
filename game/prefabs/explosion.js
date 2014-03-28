4c03d519d4f7f316ac6f2a02b1175aac62e50ee4
jdowell <jeremy@codevinsky.com>
2014-03-28 16:55:53 -0500

    BUT DAT COLLISION THO

--- game/prefabs/explosion.js
+++ game/prefabs/explosion.js
@@ -5,13 +5,10 @@ var Explosion = function(game, x, y, color, particleSize, explosionSize) {
   this.explosionSize = explosionSize || 10;
   this.particleSize = particleSize;
 
-  Phaser.Particles.Arcade.Emitter.call(this, game, 0, 0, 100);
-  this.particleClass = Block
+  Phaser.Particles.Arcade.Emitter.call(this, game, 0, 0);
+  this.particleClass = Bullet
   this.gravity = 500;
-  this.makeParticles(4,'white');
-  
-
-  
+  this.makeParticles(4,BulletTypes.PLAYER);
 };
 
 Explosion.prototype = Object.create(Phaser.Particles.Arcade.Emitter.prototype);
@@ -19,11 +16,13 @@ Explosion.prototype.constructor = Explosion;
 
 Explosion.prototype.setColor = function(color) {
   this.color = color;
-  this.callAll('setColor', null, this.color);
+  this.forEachDead(function(particle) {
+    particle.setColor(this.color);
+  }, this);
 };
 
 Explosion.prototype.explode = function(explosionSize) {
   explosionSize = explosionSize || this.explosionSize;
-  this.start(true, 2000, null, 20);
+  this.start(true, 2000, null, explosionSize);
 }
 
--- game/states/boot.js
+++ game/states/boot.js
@@ -1,3 +1,41 @@
+var BulletTypes = Object.freeze({
+  UP: {
+    color: 'yellow',
+    velocity: {
+      x: 0,
+      y: -1
+    }
+  },
+  RIGHT: {
+    color: 'green',
+    velocity: {
+      x: 1,
+      y: 0
+    }
+  },
+  DOWN: {
+    color: 'red',
+    velocity: {
+      x: 0,
+      y: 1
+    }
+  },
+  LEFT: {
+    color: 'blue',
+    velocity: {
+      x: -1,
+      y: 0
+    }
+  },
+  DEFAULT: {
+    color: 'white',
+    velocity: {
+      x: 0,
+      y: 0
+    }
+  }
+});
+
 (function () {
   'use strict';
 
